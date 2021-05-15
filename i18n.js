23066d3e53d35581ea9b4692015cb070720f31bc
Joao Araujo <jpdejavite@gmail.com>
2021-05-14 21:42:59 -0300

    feat: first version

---
+++ i18n.js
@@ -0,0 +1,16 @@
+const i18n = require('i18n');
+
+i18n.configure({
+  // setup some locales - other locales default to en silently
+  locales: ['en', 'pt-BR'],
+
+  // where to store json files - defaults to './locales' relative to modules directory
+  directory: __dirname + '/locales',
+
+  defaultLocale: 'en',
+
+  // sets a custom cookie name to parse locale settings from  - defaults to NULL
+  cookie: 'lang',
+});
+
+module.exports = i18n;
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const i18n = require('./i18n');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .use(i18n.init)
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/privacy', (req, res) => res.render('pages/privacy'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
